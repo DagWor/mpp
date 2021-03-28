fc3eb904fe2c8f37288135a7633514b1c51269fa
dagwor <dagmawiworku11@gmail.com>
2021-03-27 23:10:48 -0500

    customer create in teller

--- src/main/java/com/example/mpp/controllers/TellerController.java
+++ src/main/java/com/example/mpp/controllers/TellerController.java
@@ -1,16 +1,95 @@
 package com.example.mpp.controllers;
 
 import com.example.mpp.models.Branch;
+import com.example.mpp.models.ERole;
+import com.example.mpp.models.Role;
+import com.example.mpp.models.User;
+import com.example.mpp.payload.request.LoginRequest;
+import com.example.mpp.payload.request.SignupRequest;
+import com.example.mpp.payload.response.JwtResponse;
+import com.example.mpp.payload.response.MessageResponse;
+import com.example.mpp.repository.RoleRepository;
+import com.example.mpp.repository.UserRepository;
+import com.example.mpp.security.jwt.JwtUtils;
+import com.example.mpp.security.services.UserDetailsImpl;
+import org.apache.logging.log4j.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/teller")
 public class TellerController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PreAuthorize("hasRole('TELLER')")
+    @PostMapping("/create-customer")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Username already taken"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Account with this Email found"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+        String message = "User sign up as " + user.getRoles();
+
+        ArrayList<Role> m = new ArrayList<>(user.getRoles());
+        return ResponseEntity.ok(new MessageResponse(message));
+    }
+
 
     @GetMapping("/deposit")
     @PreAuthorize("hasRole('TELLER')")
@@ -18,13 +97,13 @@ public class TellerController {
 
     }
 
-    @GetMapping("/withdraw")
+    @PostMapping("/withdraw")
     @PreAuthorize("hasRole('TELLER')")
     public void makeWithdrawal(double amount){
 
     }
 
-    @GetMapping("/transfer")
+    @PostMapping("/transfer")
     @PreAuthorize("hasRole('TELLER')")
     public double totalDeposit(){
         Branch b = new Branch();
@@ -37,9 +116,9 @@ public class TellerController {
         return 0.0;
     }
 
-    @GetMapping("/create-transfer")
+    @PostMapping("/activate-customer")
     @PreAuthorize("hasRole('TELLER')")
-    public void createTransfer(){
+    public void createCustomer(){
 
     }
 }
