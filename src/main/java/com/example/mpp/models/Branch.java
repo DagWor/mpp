31a61577161dc5715212d8daaa56e55af1ef4459
dagwor <dagmawiworku11@gmail.com>
2021-03-29 18:00:38 -0500

    redesigning new branch

--- src/main/java/com/example/mpp/models/Branch.java
+++ src/main/java/com/example/mpp/models/Branch.java
@@ -1,8 +1,22 @@
 package com.example.mpp.models;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "branch")
 public class Branch {
 
     private int depositAmount = 98987;
+    private List<User> users;
+
+    public void addToBranch(User user){
+        users.add(user);
+    }
+
+    public List<User> getBranchUsers(){
+        return users;
+    }
 
     public int getDepositAmuont() {
         return depositAmount;
