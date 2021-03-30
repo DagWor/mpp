9f4515b698aa6fcecaf6559a5056ff52b6fc9af1
dagwor <dagmawiworku11@gmail.com>
2021-03-30 11:19:18 -0500

    branch methods and attributes defined

--- src/main/java/com/example/mpp/models/Branch.java
+++ src/main/java/com/example/mpp/models/Branch.java
@@ -1,21 +1,76 @@
 package com.example.mpp.models;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "branch")
 public class Branch {
 
     private int depositAmount = 98987;
-    private List<User> users;
 
-    public void addToBranch(User user){
-        users.add(user);
+    private List<User> customers = new ArrayList<>();
+    private List<User> tellers = new ArrayList<>();
+
+    private User admin;
+
+    public Branch(User admin){
+        this.admin = admin;
+        tellers.add(admin);
+        customers.add(admin);
+    }
+    public void addCustomerToBranch(User user){
+        customers.add(user);
     }
 
-    public List<User> getBranchUsers(){
-        return users;
+
+    public void addTellerToBranch(User user){
+        tellers.add(user);
+    }
+
+    public List<User> getBranchCustomers(){
+        return customers;
+    }
+
+    public void setBranchTellers(List<User> tellers){
+        this.tellers = tellers;
+    }
+
+    public int getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(int depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public List<User> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<User> customers) {
+        this.customers = customers;
+    }
+
+    public List<User> getTellers() {
+        return tellers;
+    }
+
+    public User getAdmin() {
+        return admin;
+    }
+
+    public List<User> getBranchTellers(){
+        return tellers;
+    }
+    public User getBranchAdmin(){
+        return admin;
+    }
+
+    public void setAdmin(User user){
+        this.admin = user;
     }
 
     public int getDepositAmuont() {
