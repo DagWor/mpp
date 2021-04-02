79846bbb8637b115b8bf25b16ff127892b2ab37f
dagwor <dagmawiworku11@gmail.com>
2021-04-02 16:25:51 -0500

    transaction repository with more search

--- src/main/java/com/example/mpp/repository/TransactionRepository.java
+++ src/main/java/com/example/mpp/repository/TransactionRepository.java
@@ -1,9 +1,13 @@
 package com.example.mpp.repository;
 
+import com.example.mpp.models.Account;
+import com.example.mpp.models.AccountInfo;
 import com.example.mpp.models.Transaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
 public interface TransactionRepository extends MongoRepository<Transaction,Integer> {
 
+    List<Transaction> findTransactionsByAccountInfo(AccountInfo accountNumber);
 
 }
