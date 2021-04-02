fd06aac2c43fb73a4c72653136dae6c76b143f6c
dagwor <dagmawiworku11@gmail.com>
2021-04-02 16:24:05 -0500

    some customer controllers included

--- src/main/java/com/example/mpp/controllers/CustomerController.java
+++ src/main/java/com/example/mpp/controllers/CustomerController.java
@@ -1,10 +1,20 @@
 package com.example.mpp.controllers;
 
+import com.example.mpp.models.Account;
+import com.example.mpp.models.AccountInfo;
 import com.example.mpp.models.Transaction;
+import com.example.mpp.models.User;
+import com.example.mpp.repository.AccountRepository;
 import com.example.mpp.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -14,6 +24,9 @@ public class CustomerController {
 
     @Autowired
     private TransactionRepository transactionRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @PostMapping("/transfer")
     @PreAuthorize("hasRole('CUSTOMER')")
@@ -28,7 +41,20 @@ public class CustomerController {
 
     }
 
+    @GetMapping("/accounts")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<List<AccountInfo>> accounts(){
+        List<AccountInfo> accounts = accountRepository.findAll();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
 
+    @GetMapping("/account/{accountNumber}")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseEntity<List<Transaction>> transactions(@PathVariable("accountNumber") int accountNumber){
+        Optional<AccountInfo> accountInfo = accountRepository.findAccountInfoByAccountNumber(accountNumber);
+        List<Transaction> transactions = transactionRepository.findTransactionsByAccountInfo(accountInfo.get());
+        return new ResponseEntity<>(transactions, HttpStatus.OK);
+    }
 
 
 }
