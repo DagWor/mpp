8b91697e134ae278a0466038309100e91157f151
dagwor <dagmawiworku11@gmail.com>
2021-04-04 00:58:53 -0500

    another search method for user

--- src/main/java/com/example/mpp/repository/UserRepository.java
+++ src/main/java/com/example/mpp/repository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.mpp.repository;
 
 import java.util.Optional;
+import java.util.Set;
+import java.util.List;
+
+import com.example.mpp.models.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.mpp.models.User;
@@ -13,4 +17,6 @@ public interface UserRepository extends MongoRepository<User, String> {
   Boolean existsByEmail(String email);
 
   User findUserByUsername(String username);
+
+  List<User> findUserByBranchNameAndRoles(String branchName, Set<Role> roles);
 }
