a05c611eda34d8a96eb05e74dfe992fd5c7a0a07
dagwor <dagmawiworku11@gmail.com>
2021-04-03 20:35:00 -0500

    accounts page modified

--- frontend/src/components/user/accounts.js
+++ frontend/src/components/user/accounts.js
@@ -60,6 +60,7 @@ export default class Accounts extends Component {
                         error.toString()
                 });
             }
+
         );
     }
 
@@ -80,6 +81,7 @@ export default class Accounts extends Component {
 
 
     render() {
+        console.log("length is : ", this.state.content.length)
             return (
             <Container>
                 <Typography style={{fontSize: 20, alignSelf: "center"}} color="textPrimary">Accounts</Typography>
@@ -99,59 +101,6 @@ export default class Accounts extends Component {
                                 <br />
                             </Typography>
                         </CardContent>
-
-                        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-                            View Transaction History
-                        </Button>
-                        <div>
-                            {value.transactionList.map(trans => {
-                            return (
-                                <Dialog fullScreen open={this.state.open} onClose={this.handleClose} TransitionComponent={Transition}>
-                                    <AppBar style={{position: 'relative'}}>
-                                        <Toolbar>
-                                            <IconButton edge="start" color="inherit" onClick={this.handleClose} aria-label="close">
-                                                <CloseIcon />
-                                            </IconButton>
-                                            <Typography variant="h6" style={{marginLeft: 2, flex: 1}}>
-                                                {value.accountNumber}
-                                            </Typography>
-                                            <Button autoFocus color="inherit" onClick={this.handleClose}>
-                                                Close
-                                            </Button>
-                                        </Toolbar>
-                                    </AppBar>
-                                    <Card style={{minWidth: 275, maxWidth: 400, margin: 7, display: "inline-block"}} variant="outlined">
-                                        <CardContent>
-                                            <Typography style={{fontSize: 14}} color="textSecondary" gutterBottom>
-                                                {trans.type}
-                                            </Typography>
-                                            <Typography variant="h5" component="h2">
-                                                $ {trans.amount}
-                                            </Typography>
-                                            {(value.type === "TRANSFER") && (
-
-                                                <Typography variant="body2" component="p">
-                                                    From Account : {trans.fromAccount}
-                                                    <br/>
-                                                    to Account : {trans.toAccount}
-                                                    <br />
-                                                </Typography>
-                                            )
-                                            }
-                                            <Typography variant="body2" component="p">
-                                                {trans.transactionDate}
-                                                <br />
-                                            </Typography>
-                                        </CardContent>
-                                        {/*<CardActions>*/}
-                                        {/*    <Button size="small">View Account Transactions</Button>*/}
-                                        {/*</CardActions>*/}
-                                    </Card>
-                                </Dialog>
-
-                            )
-                        })}
-                        </div>
                     </Card>
                 )
             })}
